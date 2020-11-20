7aa34b8734151c0e12a49089f5fac159d15fc70d
Pinza studio <67124011+dev-pinza@users.noreply.github.com>
2020-11-20 15:15:14 +0100

    MAKING UPDATE

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Cuba admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Cuba admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="description" content="Anchorb-needle Data Virtualization">
+    <meta name="keywords" content="Survey, Managment, Application, Software">
     <meta name="author" content="pixelstrap">
     <link rel="icon" href="assets/images/" type="image/x-icon">
     <link rel="shortcut icon" href="assets/images/" type="image/x-icon">
